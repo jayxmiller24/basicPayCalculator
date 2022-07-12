bae149cdfd58d8883d1fdded5581446323049f5e
Jxmiller <jxmiller5@gmail.com>
2022-07-12 14:59:00 -0700

    changed html and javascript

--- assets/javascript/calculator.js
+++ assets/javascript/calculator.js
@@ -1,63 +1,41 @@
 
 
 
-function getVal(){
+function getVal() {
 
-  let doorDashPay=document.getElementById('regPayDD').value;
-  let jobTwoPay=document.getElementById('regPayJ2').value;
-  let jobOneHours = document.getElementById('hoursDD').value;
-  let jobTwoHours=document.getElementById("hoursJ2").value;
+  //dDp is doordash pay
+  const dDP = 30;
+  //dOP is doordash overtime pay
+  const dOP = 45;
+  let hours = parseFloat(document.getElementById("hours").value);
+  let overtimeHours;
+  let regularHours;
+  let overtimePay;
+  let regularPay;
+  let totalPay;
 
+  if (hours > 80) {
 
-   let dDP= parseFloat(doorDashPay);
-   let jTP = parseFloat(jobTwoPay);
-   let jOH = parseFloat(jobOneHours);
-   let jTH = parseFloat(jobTwoHours);
-   let oTP;
-   let nP;
-   let nHO;
-   let result;
-   let resultJ2;
-   let overallTotal;
-   let mT;
-   resultJ2 = jTP * jTH;
+    overtimeHours = hours - 80;
+    regularHours = hours - overtimeHours;
+    overtimePay = overtimeHours * dOP;
+    regularPay = dDP * regularHours;
+    totalPay = overtimePay + regularPay;
 
-   
- if (dDP < 0 ){
-     alert("Please put in a number greater then 0!");
+    document.getElementById("OvertimeHours").innerHTML = overtimeHours;
+    document.getElementById("regHours").innerHTML = regularHours;
+    document.getElementById("overallPay").innerHTML = totalPay
 
- }
- else if (jOH < 0){
-     alert("Please do not put in a negative number for hours!");
+  }
+  else if (hours <= 80) {
+    
+    regularPay = dDP * hours;
+    totalPay = regularPay;
+    document.getElementById("regHours").innerHTML = hours;
+    document.getElementById("overallPay").innerHTML = totalPay;
 
- }
- 
-   else if (jOH > 80) {
-       oTP = 36.75;
-       nHO = jOH - 80;
-       nP = nHO*oTP; 
-       result = nP + (dDP*80) ;
-       overallTotal = resultJ2 + result;
-       mT = overallTotal * 2;
-       document.getElementById('doordashtotal').innerHTML = result;
-       document.getElementById('overallTotal').innerHTML = overallTotal;
-       document.getElementById('jobTwoTotal').innerHTML = resultJ2;
-       document.getElementById('monthTotal').innerHTML = mT;
-       return result;
+  } else {
+    alert("cant be less then 0!")
+  };
 
-   } else {
-      result = dDP * 80;
-      overallTotal = resultJ2 + result;
-      mT = overallTotal * 2;
-      document.getElementById('jobTwoTotal').innerHTML = resultJ2;
-      document.getElementById('doordashtotal').innerHTML = result;
-      document.getElementById('overallTotal').innerHTML = overallTotal;
-      document.getElementById('monthTotal').innerHTML = mT;
-      
-      return result;
-   }
-
- 
-  
-}
- 
+};
